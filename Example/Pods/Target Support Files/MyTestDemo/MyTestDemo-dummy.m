5d39cf4544235e37d36b0f93611e24558035b7c1
jackgx <jack.gx@webi.com.cn>
2018-11-21 15:08:40 +0800

    First version

---
+++ Example/Pods/Target Support Files/MyTestDemo/MyTestDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyTestDemo : NSObject
+@end
+@implementation PodsDummy_MyTestDemo
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyTestDemo_Tests/Pods-MyTestDemo_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyTestDemo_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MyTestDemo_Tests
+@end
